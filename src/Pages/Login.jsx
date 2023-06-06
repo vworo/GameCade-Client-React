c98bd07b7f92b6a0d6c36c580ccfd2dbf77c14d8
trstnb1998 <tristanbartolome@hotmail.com>
2023-06-06 16:10:14 +1000

    outlet bugfix

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     };
 
     return (
-        <div class="loginContainer">
+        <div className="loginContainer">
             <div className="landingContent">
                 <h1>GAMECADE</h1>
                 Username:
