dd339b2fab7284ca4759d7ef0ce5e789b3d75808
trstnb1998 <tristanbartolome@hotmail.com>
2023-06-07 22:45:45 +1000

    username added to lobby

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,21 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom'
 import ChatBox from '../components/ChatBox';
-import {useLocation, useOutletContext } from 'react-router-dom';
+import { useLocation, useOutletContext } from 'react-router-dom';
+import '../pages/Lobby.css'
 
 export default function Lobby() {
     const { state } = useLocation();
 
     useEffect(() => {
-    console.log(state);
+        console.log(state);
 
 
     }, [state])
     return (
         <div>
+            <nav>
+                <h2>{state.username}</h2>
+            </nav>
             <h1>GAMECADE</h1>
-            <ChatBox />
-            <Outlet />
+            <div>
+                <ChatBox />
+                <Outlet />
+            </div>
         </div>
     )
 }
