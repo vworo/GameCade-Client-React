6bc6fa537dc89091fd7ce25bb103f0e98a0bb59e
trstnb1998 <tristanbartolome@hotmail.com>
2023-06-08 00:10:41 +1000

    Games component added

---
+++ src/components/games/Games.jsx
@@ -0,0 +1,16 @@
+import '../games/Games.css'
+
+export default function Games() {
+    return (
+        <div>
+            <div className="game-modes">
+                <div className="game-card">
+                    <h3>WRITE AND DRAW</h3>
+                    <div className="hide">
+                        <p>Write and draw alternately until you reach the last turn.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
