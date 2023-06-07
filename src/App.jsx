705cf2678d628e4b7e5e8839c39d94f3eca65f22
vworo <victorworobec@gmail.com>
2023-06-07 22:33:17 +1000

    Changed the way Routes are setup (moved routes into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Lobby from './pages/Lobby';
 
 function App() {
     return (
         <React.Fragment>
-            <Outlet />
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/lobby' element={<Lobby />} />
+            </Routes>
         </React.Fragment>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import Login from './pages/Login.jsx';
-import Lobby from './pages/Lobby.jsx';
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: '/',
-                element: <Login />
-            },
-            {
-                path: 'lobby',
-                element: <Lobby />
-            },
-        ]
-    },
-
-]);
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
 )
