ade80b0b835cb9a3dab62dfa36d7792fcc9429cf
trstnb1998 <tristanbartolome@hotmail.com>
2023-06-13 18:29:24 +1000

    landing page styling complete

--- src/components/login/SignInGoogle.jsx
+++ src/components/login/SignInGoogle.jsx
@@ -1,6 +1,7 @@
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase.js';
+import '../login/SignInGoogle.css'
 
 export default function SignInGoogle() {
     const provider = new GoogleAuthProvider();
@@ -16,6 +17,8 @@ export default function SignInGoogle() {
     };
 
     return (
-        <button onClick={_signInWithGoogle}>Sign in with Google</button>
+        <div className="googleSignIn">
+            <button onClick={_signInWithGoogle}>SIGN IN WITH GOOGLE</button>
+        </div>
     );
 };
