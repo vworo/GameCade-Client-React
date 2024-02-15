d0cd04f6fbc7670801cde2ed6289fc2eb5d2fe77
Robinson Lam <rlam@playvox.com>
2024-02-15 17:22:31 +1100

    more styling

--- gamecade-client/src/app/layout.tsx
+++ gamecade-client/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`min-h-svh flex flex-col ${inter.className}`}>
         <SignOut />
         <RedirectToLobby />
         { children }
--- gamecade-client/src/app/page.tsx
+++ gamecade-client/src/app/page.tsx
@@ -4,12 +4,12 @@ import SignInGoogle from '../components/login/SignInGoogle';
 
 export default function Login() {
     return (
-        <div>
+        <div className="flex flex-1 flex-col align-center justify-center text-center items-center h-full">
             <h1 className="text-9xl italic font-bold mb-4">GAMECADE</h1>
             <p className="mb-2">🎮 <strong>Welcome to Gamecade:</strong> Play, earn credits, and unlock a world of rewards. <br />
                 Our online arcade offers thrilling games crafted by talented developers.
             </p>
-            <p className="mb-2">
+            <p className="mb-8">
                 Join a vibrant community, customize your experience, and level up your gaming journey. <br />
                 Ready to play, earn, and thrive? Join Gamecade now!</p>
             <SignInGoogle />
