40e724e931f91ab9481c997bfa26eb3ad1602b3e
Tristan <Tristanbartolome@hotmail.com>
2024-02-23 22:42:06 +1100

    lines deleted in App.jsx for potential build fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import Lobby from './pages/Lobby';
 import Game from './pages/Game';
 import SignOut from './components/SignOut';
@@ -13,7 +13,7 @@ function App() {
             <SignOut />
             <RedirectToLobby />
             <Routes>
-                <Route path='/' element={<Login />} />
+                {/* <Route path='/' element={<Login />} /> */}
                 <Route path='/lobby' element={<Lobby />} />
                 <Route path='/lobby/:lobbyCode' element={<Game />} />
             </Routes>
